640b5fe5ab63836894e03025f3d2398b3325f5c1
zhuning <chooin@126.com>
2017-06-23 16:34:21 +0800

    补全 toast example

--- src/views/toast/index.vue
+++ src/views/toast/index.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="v-toast">
-    <c-btn type="primary" @click="toastClick">toast</c-btn>
-    <c-btn type="primary" style="margin-top: 1000px;" @click="toastClick">toast</c-btn>
+    <c-btn type="primary" @click="toast0Click">快速 toast</c-btn>
+    <c-btn type="primary" @click="toast1Click">快速列表 toast</c-btn>
+    <c-btn type="primary" @click="toast2Click">3000 毫秒关闭，关闭回调方法</c-btn>
+    <c-btn type="primary" @click="toast3Click">列表提示 5000 毫秒关闭，关闭回调方法</c-btn>
+    <c-btn type="primary" @click="toast4Click">toast 覆盖 toast</c-btn>
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    toastClick () {
+    toast0Click () {
+      this.$Toast('toast')
+    },
+    toast1Click () {
+      this.$Toast([
+        'toast',
+        'toast1321312313213'
+      ])
+    },
+    toast2Click () {
       this.$Toast({
         texts: 'toast',
         duration: 3000,
         cb: () => {
-          console.log('cb')
+          window.alert('cb')
         }
       })
+    },
+    toast3Click () {
       this.$Toast({
         texts: [
           'toast',
@@ -23,9 +37,13 @@ export default {
         ],
         duration: 5000,
         cb: () => {
-          console.log('cb')
+          window.alert('cb')
         }
       })
+    },
+    toast4Click () {
+      this.toast2Click()
+      this.toast3Click()
     }
   }
 }

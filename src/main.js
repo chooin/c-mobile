b4d4c22133c5aec0f3863ed75b948d0a44fddb01
zhuning <chooin@126.com>
2017-06-23 15:38:38 +0800

    将 package 放在 src 目录下

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './app'
 import router from './router'
-import Mobile from '../packages'
+import Mobile from '@/packages'
 
 Vue.config.productionTip = false
 Vue.use(Mobile)

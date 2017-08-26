577d74a596c6827e82471039bec4c040e1fa01d5
zhuning <chooin@126.com>
2017-08-26 10:25:47 +0800

    支持修改主题

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # c-mobile 
 > Mobile UI elements for Vue 2.0
 
-### Install
+### 安装
 
 ``` sh
 yarn add c-mobile
@@ -9,12 +9,42 @@ yarn add c-mobile
 npm install c-mobile --save
 ```
 
-### Usage
+### 使用
 
 ``` js
 import Vue from 'vue'
 import Mobile from 'c-mobile'
 import 'c-mobile/dist/index.css'
+
+Vue.use(Mobile)
+```
+
+### 自定义主题
+
+在项目下建立一个 scss 文件 index.scss，并写入下面内容：
+
+``` scss
+// 常用颜色
+$primary: #39f;
+$success: #0c6;
+$warning: #f90;
+$danger: #f30;
+
+// 文字颜色
+$text: #737373;
+$text-light: #a6a6a6;
+
+// 边框颜色
+$border: #d9d9d9;
+@import '~c-mobile/dist/index.scss';
+```
+
+然后在入口文件 main.js 内导入这个 scss 文件即可：
+
+``` js
+import Vue from 'vue'
+import Mobile from 'c-mobile'
+import './index.scss'
 
 Vue.use(Mobile)
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-var gulp = require('gulp');
-var cleanCSS = require('gulp-clean-css');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require('gulp')
+var cleanCSS = require('gulp-clean-css')
+var sass = require('gulp-sass')
+var rename = require('gulp-rename')
+var autoprefixer = require('gulp-autoprefixer')
 
 // 编译 sass
 gulp.task('css', function () {
-  gulp.src('./packages/index.scss')
+  gulp.src('./packages/include-variables.scss')
       .pipe(sass())
       .pipe(autoprefixer({
         browsers: ['last 2 versions', 'ie > 8']
       }))
       .pipe(cleanCSS())
       .pipe(rename('index.css'))
-      .pipe(gulp.dest('./dist'));
-});
+      .pipe(gulp.dest('./dist'))
+})
 
-gulp.task('default', ['css']);
+gulp.task('default', ['css'])

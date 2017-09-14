7a7c15a09cbf3a713ee083c394bc53d68e697920
zhuning <chooin@126.com>
2017-09-14 13:54:36 +0800

    add: 跳转支持直接跳转到当前域名下任意地址

--- packages/units/to.js
+++ packages/units/to.js
@@ -1,6 +1,6 @@
 export default t => {
   if (t.to) {
-    if ((typeof t.to === 'string' && t.to.indexOf('/') === 0) || typeof t.to === 'object') {
+    if ((typeof t.to === 'string' && t.to.indexOf('/') === 0 && !t.to.indexOf('//')) || typeof t.to === 'object') {
       t.$router.push(t.to)
     } else {
       window.location.href = t.to

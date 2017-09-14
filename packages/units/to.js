71e59f80590247b1427277f8f54ad2f086fad94a
zhuning <chooin@126.com>
2017-09-14 14:07:13 +0800

    add: 跳转支持直接跳转到当前域名下任意地址

--- packages/units/to.js
+++ packages/units/to.js
@@ -1,9 +1,13 @@
 export default t => {
   if (t.to) {
-    if ((typeof t.to === 'string' && t.to.indexOf('/') === 0 && !t.to.indexOf('//')) || typeof t.to === 'object') {
-      t.$router.push(t.to)
+    if (typeof t.to === 'string' && t.to.indexOf('//') === 0) {
+      window.location.href = t.to.substring(1)
     } else {
-      window.location.href = t.to
+      if ((typeof t.to === 'string' && t.to.indexOf('/') === 0) || typeof t.to === 'object') {
+        t.$router.push(t.to)
+      } else {
+        window.location.href = t.to
+      }
     }
   } else {
     t.$emit('click')
--- src/views/text/index.vue
+++ src/views/text/index.vue
@@ -10,6 +10,7 @@
     <c-text block to="/">跳转到首页</c-text>
     <c-text block to="http://www.163.com">跳转到网易</c-text>
     <c-text block to="tel:13888888888">打电话</c-text>
+    <c-text block to="//example">跳转到当前域名下的某个地址</c-text>
     <c-text block @click="alert">事件</c-text>
     <c-text block light>浅灰色</c-text>
   </div>

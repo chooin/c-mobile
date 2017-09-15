52bef7733362c736922cf43286c334244cce12e5
zhuning <chooin@126.com>
2017-09-15 15:44:28 +0800

    change: gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
-var gulp = require('gulp')
-var cleanCSS = require('gulp-clean-css')
-var sass = require('gulp-sass')
-var rename = require('gulp-rename')
-var autoprefixer = require('gulp-autoprefixer')
+var gulp = require('gulp'),
+    cleanCSS = require('gulp-clean-css'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer'),
+    copy = require('gulp-copy')
+    del = require('del')
 
 // 编译 sass
 gulp.task('css', function () {
-  gulp.src('./packages/include-variables.scss')
-      .pipe(sass())
-      .pipe(autoprefixer({
-        browsers: ['last 2 versions', 'ie > 8']
-      }))
-      .pipe(cleanCSS())
-      .pipe(rename('index.css'))
-      .pipe(gulp.dest('./dist'))
+  gulp
+    .src('./packages/include-variables.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'ie > 8']
+    }))
+    .pipe(cleanCSS())
+    .pipe(rename('index.css'))
+    .pipe(gulp.dest('./dist'))
+  gulp
+    .src(['./dist/packages/index.js', './packages/index.scss'])
+    .pipe(gulp.dest('dist/'))
+  del(['./dist/packages'])
 })
 
 gulp.task('default', ['css'])

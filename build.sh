c443399875b6b1541ba5b39867af7817d0cb64b5
zhuning <chooin@126.com>
2017-09-29 17:48:23 +0800

    fix: build.sh

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@ echo "请选择需要编译的环境: (\033[35m默认 2\033[0m)"
 echo "\t \033[35m 1) \033[0m 编译 c-mobile"
 echo "\t \033[35m 2) \033[0m 编译 web"
 echo "\t \033[35m 0) \033[0m 退出"
-read buildEnv
+read BUILD_ENV
 
-if [ $buildEnv -eq '0' ]
+if [ "$BUILD_ENV" = '0' ]
 then
   exit;
 fi
 
-case "$buildEnv" in
+case "$BUILD_ENV" in
   1)
     rm -rf ./dist/*
     yarn

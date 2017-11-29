d9eccc29779a5a5e9a0a74449a4c1d21cfca1cb8
zhuning <chooin@126.com>
2017-11-29 19:24:45 +0800

    update: c-flex 支持 to

--- packages/components/flex/flex.vue
+++ packages/components/flex/flex.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="c-flex">
+  <div
+    class="c-flex"
+    @click="handleClick">
     <slot></slot>
   </div>
 </template>
@@ -11,6 +13,15 @@ export default {
     gutter: {
       type: Number,
       default: 0
+    },
+    to: {
+      type: [String, Object],
+      default: null
+    }
+  },
+  methods: {
+    handleClick () {
+      to(this)
     }
   }
 }

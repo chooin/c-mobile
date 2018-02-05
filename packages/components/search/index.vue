66c456e48d2626ee45c05577a36b908236652484
zhuning <chooin@126.com>
2018-02-05 21:21:55 +0800

    update: c-search cancel icon

--- packages/components/search/index.vue
+++ packages/components/search/index.vue
@@ -8,7 +8,7 @@
       v-model="_value"
       @keyup="inputKeyupEnter">
     <i class="c-search__cancel-button" v-if="_value" @click="deleteClick">
-      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
         <path fill="#bbb" d="M17.016 15.609l-3.609-3.609 3.609-3.609-1.406-1.406-3.609 3.609-3.609-3.609-1.406 1.406 3.609 3.609-3.609 3.609 1.406 1.406 3.609-3.609 3.609 3.609zM12 2.016c5.531 0 9.984 4.453 9.984 9.984s-4.453 9.984-9.984 9.984-9.984-4.453-9.984-9.984 4.453-9.984 9.984-9.984z"></path>
       </svg>
     </i>

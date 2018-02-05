5dc7dddbbb4c37974fbf0780aaf9a586d4447359
zhuning <chooin@126.com>
2018-02-05 21:30:47 +0800

    update:

--- packages/components/search/index.vue
+++ packages/components/search/index.vue
@@ -6,7 +6,7 @@
       class="c-search__input"
       :placeholder="placeholder"
       v-model="_value"
-      @keyup="inputKeyupEnter">
+      @keyup="inputKeyup">
     <i class="c-search__cancel-button" v-if="_value" @click="deleteClick">
       <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
         <path fill="#bbb" d="M17.016 15.609l-3.609-3.609 3.609-3.609-1.406-1.406-3.609 3.609-3.609-3.609-1.406 1.406 3.609 3.609-3.609 3.609 1.406 1.406 3.609-3.609 3.609 3.609zM12 2.016c5.531 0 9.984 4.453 9.984 9.984s-4.453 9.984-9.984 9.984-9.984-4.453-9.984-9.984 4.453-9.984 9.984-9.984z"></path>
@@ -36,7 +36,7 @@ export default {
     this.autofocus && this.$refs.search.focus()
   },
   methods: {
-    inputKeyupEnter (keyCode) {
+    inputKeyup (keyCode) {
       if (keyCode.key === 'Enter') this.$emit('keyup-enter', this._value)
     },
     deleteClick () {

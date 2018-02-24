d3a9212ae2b9004b922c047060ece31b3ba36b2d
zhuning <chooin@126.com>
2018-02-24 13:40:37 +0800

    update: actionsheet 样式

--- src/views/actionsheet/index.vue
+++ src/views/actionsheet/index.vue
@@ -8,12 +8,13 @@
     <c-actionsheet
       :actions="actionsheet.actions"
       title="2017年12月13日，谷歌正式宣布谷歌AI中国中心（Google AI China Center）在北京成立"
-      description="1999年下半年，谷歌网站“Google”正式启用。[3]  2010年3月23日，宣布关闭在中国大陆市场搜索服务。2015年8月10日，宣布对企业架构进行调整，并创办了一家名为Alphabet的“伞形公司”（Umbrella Company），成为Alphabet旗下子公司。2015年，在2015年度“世界品牌500强”排行中重返榜首，苹果和亚马逊分别位居第二和第三名。2016年6月8日，《2016年BrandZ全球最具价值品牌百强榜》公布，以2291.98亿美元的品牌价值重新超越苹果成为百强第一。[4]  2017年2月，Brand Finance发布2017年度全球500强品牌榜单，排名第一。[5]  2017年6月，《2017年BrandZ最具价值全球品牌100强》公布，谷歌公司名列第一位。"
+      description="1999年下半年，谷歌网站“Google”正式启用。[3]  2010年3月23日，宣布关闭在中国大。"
       cancel-text="取消"
       v-model="actionsheet.visible" />
     <c-actionsheet
       :actions="actionsheetnone.actions"
-      cancel-text="取消"
+      title="Actionsheet"
+      cancel-text="Cancel"
       v-model="actionsheetnone.visible" />
   </div>
 </template>
@@ -90,13 +91,13 @@ export default {
         visible: true,
         actions: [
           {
-            text: '修改',
+            text: 'Edit',
             click: () => {
               window.alert(`修改被点击`)
             }
           },
           {
-            text: '删除',
+            text: 'Delete',
             color: '#f30',
             click: () => {
               window.alert(`删除被点击`)

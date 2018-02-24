b1b2e903be14427f4307c9948570355dfc331500
zhuning <chooin@126.com>
2018-02-24 13:48:13 +0800

    update: build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo "请选择需要编译的环境: (\033[35m默认 web\033[0m)"
 echo "\t \033[35m 1) \033[0m 编译 c-mobile"
-echo "\t \033[35m 2) \033[0m 编译 web"
+echo "\t \033[35m 2) \033[0m 编译 web，并发布"
 echo "\t \033[35m 0) \033[0m 退出"
 read BUILD_ENV
 
@@ -20,5 +20,6 @@ case "$BUILD_ENV" in
   *)
     yarn
     yarn run build:web
+    cdn deploy production
     ;;
 esac

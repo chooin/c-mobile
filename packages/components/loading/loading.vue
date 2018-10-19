d17f322d56edb4e8b208cd0fee8c0e381b9d11bd
zhuning <chooin@126.com>
2018-10-19 11:08:30 +0800

    update: loading 支持文本

--- packages/components/loading/loading.vue
+++ packages/components/loading/loading.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="c-loading">
-    <i class="c-loading__loading"></i>
+    <div class="c-loading__cell">
+      <i class="c-loading__loading"></i>
+      <span v-if="text">{{ text }}</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'cLoading'
+  name: 'cLoading',
+  props: {
+    text: {
+      type: String,
+      default: null
+    }
+  }
 }
 </script>

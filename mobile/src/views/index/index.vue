afbcb4f2d10736424a5e5dada3fccdd5ab3dc2ad
zhuning <chooin@126.com>
2018-10-25 15:58:37 +0800

    update: example index 页面

--- mobile/src/views/index/index.vue
+++ mobile/src/views/index/index.vue
@@ -1,136 +1,49 @@
 <template>
   <c-container
+    padding="15px 0"
     safe-area>
     <c-input-group>
-      <c-input next addon to="/button">
-        <c-input-head>
-          Button
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/flex">
-        <c-input-head>
-          Flex
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/header">
-        <c-input-head>
-          Header
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/switch">
-        <c-input-head>
-          Switch
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/input-group">
-        <c-input-head>
-          InputGroup
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/icon">
-        <c-input-head>
-          Icon
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/tab">
-        <c-input-head>
-          Tab
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/tabbar">
-        <c-input-head>
-          Tabbar
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/toast">
-        <c-input-head>
-          Toast
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/confirm">
-        <c-input-head>
-          Confirm
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/actionsheet">
-        <c-input-head>
-          Actionsheet
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/sidebar">
-        <c-input-head>
-          Sidebar
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/text">
-        <c-input-head>
-          Text
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/loading">
-        <c-input-head>
-          Loading
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/search">
-        <c-input-head>
-          Search
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/keyboard">
-        <c-input-head>
-          Keyboard
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/radio">
-        <c-input-head>
-          RadioGroup
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
-      </c-input>
-      <c-input next addon to="/container">
-        <c-input-head>
-          container
-        </c-input-head>
-        <c-input-body>
-        </c-input-body>
+      <c-input
+        v-for="(item, index) in components"
+        :key="index"
+        :to="`/${item}`"
+        next
+        addon>
+        <c-input-body>{{ item }}</c-input-body>
       </c-input>
     </c-input-group>
   </c-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      components: [
+        'button',
+        'flex',
+        'header',
+        'switch',
+        'input-group',
+        'icon',
+        'tab',
+        'tabbar',
+        'toast',
+        'confirm',
+        'actionsheet',
+        'sidebar',
+        'text',
+        'loading',
+        'search',
+        'keyboard',
+        'radio',
+        'container',
+      ]
+    }
+  }
+}
+</script>
+
 
 <style scoped lang="scss">
   @import './style.scss';

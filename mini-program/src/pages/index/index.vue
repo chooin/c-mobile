deca40d61a64625c69a8932ff6b29f8c13cabf85
zhuning <chooin@126.com>
2019-01-13 10:51:46 +0800

    update: 小程序 c-input active

--- mini-program/src/pages/index/index.vue
+++ mini-program/src/pages/index/index.vue
@@ -1,20 +1,13 @@
 <template>
   <div class="p-index">
-    <c-button :to="{
-      path: '/312312',
-      query: {
-        aa: 1
-      }
-    }">Hello</c-button>
+    413421
   </div>
 </template>
 
 <script>
-import cButton from '@/packages/components/button/index'
 
 export default {
   components: {
-    cButton
   },
   methods: {
   }

b0933b2724b22017bbbad8ea41f783f3da552ac1
zhuning <chooin@126.com>
2019-01-25 12:06:48 +0800

    update: area

--- packages/components/picker/picker-region.vue
+++ packages/components/picker/picker-region.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    v-if="visible"
     class="c-picker-region"
     :class="{
       'is-iphonex': isMiniProgramIsIPhoneX
@@ -14,16 +15,16 @@
     </div>
     <picker-view
       indicator-class="indicator-style"
-      :value="_value"
+      :value="indexs"
       @change="onChange">
       <picker-view-column>
-        <div v-for="(item, index) in province" :key="index">{{ item.name }}</div>
+        <div v-for="(item, index) in provinces" :key="index">{{ item.name }}</div>
       </picker-view-column>
       <picker-view-column>
-        <div v-for="(item, index) in city" :key="index">{{ item.name }}</div>
+        <div v-for="(item, index) in cities" :key="index">{{ item.name }}</div>
       </picker-view-column>
       <picker-view-column>
-        <div v-for="(item, index) in district" :key="index">{{ item.name }}</div>
+        <div v-for="(item, index) in districts" :key="index">{{ item.name }}</div>
       </picker-view-column>
     </picker-view>
   </div>
@@ -41,14 +42,15 @@ export default {
   data () {
     return {
       isMiniProgramIsIPhoneX,
-      province: [],
-      city: [
+      indexs: [],
+      provinces: [],
+      cities: [
         {
           id: 0,
           name: '市'
         }
       ],
-      district: [
+      districts: [
         {
           id: 0,
           name: '区'
@@ -57,34 +59,71 @@ export default {
     }
   },
   props: {
-    value: {
-      type: Array,
-      default: () => [110000, 110100, 110101]
+    visible: {
+      type: Number,
+      default: true
+    },
+    provinceId: {
+      type: Number,
+      default: null
+    },
+    cityId: {
+      type: Number,
+      default: null
+    },
+    districtId: {
+      type: Number,
+      default: null
     }
   },
   mounted () {
-    this.setProvice()
+    this.initData()
   },
   methods: {
+    initData () {
+      let indexs = this.getIndexs([
+        this.provinceId,
+        this.cityId,
+        this.districtId
+      ])
+
+      this.setProvices(indexs)
+      this.setCities(indexs)
+      this.setDistricts(indexs)
+
+      setTimeout(() => {
+        this.indexs = indexs
+      }, 100)
+    },
     onCancel () {},
-    onDone () {},
+    onDone () {
+      if (
+        this.indexs[0] &&
+        this.indexs[1] &&
+        this.indexs[2]
+      ) {
+        let ids = this.getIds(this.indexs)
+
+        this.$emit('change', ids)
+      }
+    },
     onChange (e) {
-      if (e.detail.value[2] !== this._value[2]) {
-        this._value = e.detail.value
+      if (e.detail.value[2] !== this.indexs[2]) {
+        this.indexs = e.detail.value
         return
       }
-      if (e.detail.value[1] !== this._value[1]) {
+      if (e.detail.value[1] !== this.indexs[1]) {
         this.isCityChange(e)
-        this._value = [
+        this.indexs = [
           e.detail.value[0],
           e.detail.value[1],
           0
         ]
         return
       }
-      if (e.detail.value[0] !== this._value[0]) {
+      if (e.detail.value[0] !== this.indexs[0]) {
         this.isProvinceChange(e)
-        this._value = [
+        this.indexs = [
           e.detail.value[0],
           0,
           0
@@ -92,8 +131,17 @@ export default {
         return
       }
     },
-    setProvice () {
-      this.province = [
+    isProvinceChange (e) {
+      this.setCities(e.detail.value)
+    },
+    isCityChange (e) {
+      this.setDistricts(e.detail.value)
+    },
+    isDistrictChange () {
+
+    },
+    setProvices () {
+      this.provinces = [
         {
           id: 0,
           name: '省'
@@ -101,70 +149,70 @@ export default {
         ...originProvince
       ]
     },
-    isProvinceChange (e) {
-      this.setCity(e.detail.value)
-    },
-    isCityChange (e) {
-      this.setDistrict(e.detail.value)
-    },
-    isDistrictChange () {
-
-    },
-    setCity (value) {
-      this.city = [{
+    setCities (indexs) {
+      this.cities = [{
         id: 0,
         name: '市'
       }]
-      this.district = [{
+      this.districts = [{
         id: 0,
         name: '区'
       }]
-      let province = this.province[value[0]]
+      let province = this.provinces[indexs[0]]
       if (province) {
         let preId = province.id / 10000
-        let city = [{
+        let cities = [{
           id: 0,
           name: '市'
         }]
         for (let item of originCity) {
           if (String(item.id).indexOf(preId) === 0) {
-            city.push(item)
+            cities.push(item)
           }
         }
-        this.city = city
+        this.cities = cities
       }
     },
-    setDistrict (value) {
-      this.district = [{
+    setDistricts (indexs) {
+      this.districts = [{
         id: 0,
         name: '区'
       }]
-      let city = this.city[value[1]]
+      let city = this.cities[indexs[1]]
       if (city) {
         let preId = city.id / 100
-        let district = [{
+        let districts = [{
           id: 0,
           name: '区'
         }]
         for (let item of originDistrict) {
           if (String(item.id).indexOf(preId) === 0) {
-            district.push(item)
+            districts.push(item)
           }
         }
-        this.district = district
+        this.districts = districts
       }
     },
     getIndexs (ids) {
       let indexs = []
       if (ids[0]) {
-        let provinces = originProvince
+        let provinces = [
+          {
+            id: 0,
+            name: '省'
+          },
+          ...originProvince
+        ]
         let index = provinces.findIndex(item => item.id === ids[0])
         indexs.push(index < 0 ? 0 : index)
       } else {
         indexs.push(0)
       }
       if (ids[1]) {
-        let cities = []
+        let cities = [{
+          id: 0,
+          name: '市'
+        }]
         let pre = parseInt(ids[1] / 10000)
         for (let item of originCity) {
           if (String(item.id).indexOf(pre) === 0) {
@@ -177,7 +225,10 @@ export default {
         indexs.push(0)
       }
       if (ids[2]) {
-        let districts = []
+        let districts = [{
+          id: 0,
+          name: '区'
+        }]
         let pre = parseInt(ids[2] / 100)
         for (let item of originDistrict) {
           if (String(item.id).indexOf(pre) === 0) {
@@ -192,53 +243,11 @@ export default {
       return indexs
     },
     getIds (indexs) {
-      console.log(indexs, 'indexs')
-      let ids = []
-      if (indexs[0]) {
-        let provinces = originProvince
-        ids.push(provinces[indexs[0]].id)
-      } else {
-        ids.push(null)
-      }
-      if (indexs[1]) {
-        let cities = []
-        let pre = parseInt(ids[0] / 10000)
-        for (let item of originCity) {
-          if (String(item.id).indexOf(pre) === 0) {
-            cities.push(item)
-          }
-        }
-        ids.push(cities[indexs[1]].id)
-      } else {
-        ids.push(null)
-      }
-      if (indexs[2]) {
-        let districts = []
-        let pre = parseInt(ids[1] / 100)
-        for (let item of originDistrict) {
-          if (String(item.id).indexOf(pre) === 0) {
-            districts.push(item)
-          }
-        }
-        ids.push(districts[indexs[2]].id)
-      } else {
-        ids.push(null)
-      }
-      return ids
-    }
-  },
-  computed: {
-    _value: {
-      get () {
-        let indexs = this.getIndexs(this.value)
-        return indexs
-      },
-      set (indexs) {
-        let ids = this.getIds(indexs)
-        console.log(ids, 'ids')
-        this.$emit('input', ids)
-        this.$emit('change', ids)
-      }
+      return [
+        this.provinces[indexs[0]].id,
+        this.cities[indexs[1]].id,
+        this.districts[indexs[2]].id
+      ]
     }
   }
 }

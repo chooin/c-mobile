6618c7902fe513e56287fcbe829b89fa6b7ea636
Devin.Zhu <chooin@126.com>
2020-06-25 00:22:48 +0800

    chore: 移除无用的

--- types/btn.d.ts
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-export declare interface Btn extends Vue {
-  type: 'primary' | 'success' | 'warning' | 'danger'
-
-  disabled: boolean
-}
--- types/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { Btn } from './btn'
